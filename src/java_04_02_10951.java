8a6978511625920009711312ba7632e2b487c4e2
Jaecheol Jeong <paneko3@gmail.com>
2022-01-19 02:09:22 +0900

    2022-01-19

---
+++ src/java_04_02_10951.java
@@ -0,0 +1,16 @@
+
+// java_04_02_10951
+
+import java.util.*;
+
+public class java_04_02_10951 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		while (sc.hasNext()) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a + b);
+		}
+	}
+}
