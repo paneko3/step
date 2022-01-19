dd77cdc806d3edb804e6e0a60e80996dbaee321b
Jaecheol Jeong <paneko3@gmail.com>
2022-01-20 03:53:38 +0900

    2022-01-20

---
+++ src/java_05_04_3052.java
@@ -0,0 +1,27 @@
+
+// java_05_04_3052
+
+import java.util.*;
+
+public class java_05_04_3052 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[10];
+		int m[] = new int[42];
+
+		for (int i = 0; i < 10; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for (int i = 0; i < 10; i++) {
+			m[arr[i] % 42]++;
+		}
+		int count = 0;
+		for (int i = 0; i < 42; i++) {
+			if (m[i] > 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+
+	}
+}
