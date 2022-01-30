3e84bbb44ba7538b8577879d4ae8e36621c77caa
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-01-30 23:54:43 +0900

    Add files via upload

---
+++ src/Java_01_10_10926.java
@@ -0,0 +1,15 @@
+
+// Java_01_10_10926
+
+import java.util.*;
+
+public class Java_01_10_10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String t = sc.next();
+		System.out.println(t +"??!");
+
+		sc.close();
+	}
+}
